36ce48dc6b9166cad528665b6e1faf1cccf5c47e
Alejandro Mery <amery@geeks.cl>
2014-05-04 15:34:22 +0000

    net: add Listen() function to be smarter on the address string
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ net/listen.go
@@ -0,0 +1,27 @@
+package net
+
+import (
+	stdnet "net"
+	"os"
+	"strings"
+)
+
+func Listen(addr string) (stdnet.Listener, error) {
+	dom := "tcp"
+
+	// let the standard Listen return any error, not us
+	if len(addr) > 0 {
+		if addr[1] == '/' || addr[1:2] == "./" || addr[1:3] == "../" {
+			dom = "unix"
+		} else if strings.HasPrefix(addr, "unix:") || strings.HasPrefix(addr, "file:") {
+			dom = "unix"
+			addr = addr[5:]
+		}
+	}
+
+	if dom == "unix" {
+		_ = os.Remove(addr)
+	}
+
+	return stdnet.Listen(dom, addr)
+}
