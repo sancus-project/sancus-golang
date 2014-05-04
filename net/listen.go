3b8168be7490bade18702f85c446dbce7bc0f56e
Alejandro Mery <amery@geeks.cl>
2014-05-04 20:56:04 +0000

    net.Listen: use network instead of domain to refer to the network family of the socket
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- net/listen.go
+++ net/listen.go
@@ -9,21 +9,21 @@ import (
 type Listener stdnet.Listener
 
 func Listen(addr string) (Listener, error) {
-	dom := "tcp"
+	network := "tcp"
 
 	// let the standard Listen return any error, not us
 	if len(addr) > 0 {
 		if addr[1] == '/' || addr[1:2] == "./" || addr[1:3] == "../" {
-			dom = "unix"
+			network = "unix"
 		} else if strings.HasPrefix(addr, "unix:") || strings.HasPrefix(addr, "file:") {
-			dom = "unix"
+			network = "unix"
 			addr = addr[5:]
 		}
 	}
 
-	if dom == "unix" {
+	if network == "unix" {
 		_ = os.Remove(addr)
 	}
 
-	return stdnet.Listen(dom, addr)
+	return stdnet.Listen(network, addr)
 }
