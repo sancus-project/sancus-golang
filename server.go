85ea7d20a05000ead6b45121fdfa016fe2f03097
Alejandro Mery <amery@geeks.cl>
2014-05-04 19:17:08 +0000

    Server: Add `net/http`.Server wrapper calling our net.Listen()
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ server.go
@@ -0,0 +1,31 @@
+package web
+
+import (
+	"go.sancus.io/sancus/net"
+	"net/http"
+)
+
+// `net/http`.Server wrapper
+// to use if `sancus/net`.Listen() is wanted
+type Server struct {
+	http.Server
+}
+
+func NewServer(addr string, h http.Handler) *Server {
+	return &Server{Server: http.Server{
+		Addr:    addr,
+		Handler: h,
+	}}
+}
+
+func (s *Server) ListenAndServe() error {
+	addr := s.Addr
+	if addr == "" {
+		addr = ":http"
+	}
+	l, e := net.Listen(addr)
+	if e != nil {
+		return e
+	}
+	return s.Serve(l)
+}
