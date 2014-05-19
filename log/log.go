8dea15c320cf2f461ae784ec0bcf0cf36dc87d70
Alejandro Mery <amery@geeks.cl>
2014-05-19 23:27:59 +0000

    log.LoggerMap: singleton for Loggers, by Tag.
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- log/log.go
+++ log/log.go
@@ -12,3 +12,16 @@ const (
 	WTF
 	ASSERT
 )
+
+// LoggerMap
+var loggers = NewLoggerMap(INFO)
+
+func SetLevel(l LogLevel) {
+	loggers.Level = l
+}
+func Level() LogLevel {
+	return loggers.Level
+}
+func GetLogger(tag string) *Logger {
+	return loggers.Get(tag)
+}
---
+++ log/map.go
@@ -0,0 +1,29 @@
+package log
+
+import (
+	"sync"
+)
+
+type LoggerMap struct {
+	sync.Mutex
+
+	Level LogLevel
+	m     map[string]*Logger
+}
+
+// Constructor
+func NewLoggerMap(level LogLevel) *LoggerMap {
+	return &LoggerMap{Level: level, m: make(map[string]*Logger)}
+}
+
+// Methods
+func (m *LoggerMap) Get(tag string) *Logger {
+	m.Lock()
+	logger, ok := m.m[tag]
+	if !ok {
+		logger = &Logger{Tag: tag, Level: m.Level}
+		m.m[tag] = logger
+	}
+	m.Unlock()
+	return logger
+}
