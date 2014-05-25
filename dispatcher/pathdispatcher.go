81fddb814b5acc4867e79cc16bbfc19f88fe5c76
Alejandro Mery <amery@geeks.cl>
2014-05-25 22:32:39 +0000

    dispatcher.PathDispatcher: remove unneeded Add*() methods
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- dispatcher/pathdispatcher.go
+++ dispatcher/pathdispatcher.go
@@ -20,18 +20,5 @@ func (d *PathDispatcher) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
-// Handlers
-func (d *PathDispatcher) AddNamed(name string, pattern string, handler http.Handler) {
-}
-
-func (d *PathDispatcher) Add(pattern string, handler http.Handler) {
-	d.AddNamed("", pattern, handler)
-}
-
-func (d *PathDispatcher) AddFuncNamed(name string, pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	d.AddNamed(name, pattern, http.HandlerFunc(handler))
-}
-
-func (d *PathDispatcher) AddFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	d.AddNamed("", pattern, http.HandlerFunc(handler))
+func (d *PathDispatcher) AddHandler(name string, pattern string, handler http.Handler) {
 }
