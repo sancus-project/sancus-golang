2abe70cfbe7c969517fffde3883839aced828bbc
Alejandro Mery <amery@geeks.cl>
2014-05-31 13:05:18 +0000

    uritemplate.parser: use sublogger for the lexer
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- uritemplate/parser.go
+++ uritemplate/parser.go
@@ -6,8 +6,12 @@ import (
 
 type Template struct{}
 
+// Turn string into Template
 func ParseTemplate(template string, l *log.Logger) *Template {
-	lex := newLexer(template, l)
+	lexLogger := l.SubLogger(".lexer")
+	lexLogger.Level = log.INFO
+	lex := newLexer(template, lexLogger)
+
 	for {
 		t := lex.nextToken()
 		l.Trace("ParseTemplate: %s", t)
