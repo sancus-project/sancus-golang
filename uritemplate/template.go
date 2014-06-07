644d2f1dc031362cbfe7e4b0749c35fc0cedc689
Alejandro Mery <amery@geeks.cl>
2014-06-07 18:01:22 +0000

    uritemplate.Template: add initial support for literals
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- uritemplate/template.go
+++ uritemplate/template.go
@@ -1,11 +1,26 @@
 package uritemplate
 
 import (
+	"fmt"
 	"go.sancus.io/core/log"
 )
 
+type exprType int
+
+type expression interface {
+	Type() exprType
+	String() string
+}
+
+const (
+	exprLiteral exprType = iota + 1
+)
+
+// Template
 type Template struct {
 	logger *log.Logger
+
+	expr []expression
 }
 
 func NewTemplate(tmpl string, logger *log.Logger) (*Template, error) {
@@ -16,4 +31,27 @@ func NewTemplate(tmpl string, logger *log.Logger) (*Template, error) {
 		return nil, err
 	}
 	return t, nil
+}
+
+func (t *Template) append(e expression) {
+	t.expr = append(t.expr, e)
+}
+
+// Literal string
+//
+type tmplLiteral struct {
+	literal string
+}
+
+func (t *tmplLiteral) Type() exprType {
+	return exprLiteral
+}
+
+func (t *tmplLiteral) String() string {
+	return fmt.Sprintf("%q", t.literal)
+}
+
+func (t *Template) appendLiteral(str string) {
+	e := tmplLiteral{literal: str}
+	t.append(&e)
 }
