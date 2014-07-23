27d68fee5cd4b7732c6f6ac975e0546b1763d484
Alejandro Mery <amery@geeks.cl>
2014-07-23 23:53:19 +0000

    dispatcher: handler for Arguments on ContextMap
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ dispatcher/context.go
@@ -0,0 +1,29 @@
+package dispatcher
+
+import (
+	"fmt"
+	"go.sancus.io/web/context"
+	"net/http"
+)
+
+var (
+	DispatcherArgsLabel = "dispatcher-arguments"
+)
+
+type Arguments map[string]interface{}
+
+//
+func GetArguments(m *context.ContextMap, r *http.Request) Arguments {
+	var args Arguments
+	label := DispatcherArgsLabel
+
+	v, ok := m.Get(r, label)
+	if v == nil {
+		args = make(Arguments)
+		m.Set(r, label, args)
+	} else if args, ok = v.(Arguments); !ok {
+		panic(fmt.Sprintf("%s: already in use by incorrect data", label))
+	}
+
+	return args
+}
