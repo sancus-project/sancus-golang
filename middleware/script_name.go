7bd2949212b9c65efcf7abc84e1c65effa94879e
Alejandro Mery <amery@geeks.cl>
2014-07-30 00:17:58 +0000

    middleware: replace Bridge dispatcher with ScriptName middleware
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ middleware/script_name.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"go.sancus.io/web/context"
+	"go.sancus.io/web/dispatcher"
+	"net/http"
+	"regexp"
+)
+
+func ScriptName(ctx context.RequestContextMapper, scriptName string, h http.Handler) http.Handler {
+	if scriptName == "" || scriptName == "/" {
+		scriptName = "^()(/.*)$"
+	} else {
+		scriptName = "^(" + scriptName + ")(/.*)$"
+	}
+
+	pattern := regexp.MustCompile(scriptName)
+	f := func(w http.ResponseWriter, r *http.Request) {
+		dispatcher.Prepare(ctx, r)
+		if m := pattern.FindStringSubmatch(r.URL.Path); m != nil {
+			if m[1] != "" {
+				str := dispatcher.GetStringArgument(ctx, r, "SCRIPT_NAME", "") + m[1]
+				dispatcher.SetArgument(ctx, r, "SCRIPT_NAME", str)
+				r.URL.Path = m[2]
+			}
+			h.ServeHTTP(w, r)
+		} else {
+			http.NotFound(w, r)
+		}
+	}
+	return http.HandlerFunc(f)
+}
