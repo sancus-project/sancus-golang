bd06c5e564438b5b923fe32cc22b553f09462006
Alejandro Mery <amery@geeks.cl>
2015-05-12 01:44:12 +0200

    pty: pseudo-terminal opener
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ pty/pty.go
@@ -0,0 +1,58 @@
+package pty
+
+import (
+	"C"
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+func Open() (master, slave *os.File, err error) {
+	ptm, err := os.OpenFile("/dev/ptmx", os.O_RDWR, 0)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pts_name, err := ptsname(ptm)
+	if err != nil {
+		ptm.Close()
+		return nil, nil, err
+	}
+
+	err = unlockpt(ptm)
+	if err != nil {
+		ptm.Close()
+		return nil, nil, err
+	}
+
+	pts, err := os.OpenFile(pts_name, os.O_RDWR|syscall.O_NOCTTY, 0)
+	if err != nil {
+		ptm.Close()
+		return nil, nil, err
+	}
+
+	return ptm, pts, nil
+}
+
+func ptsname(ptm *os.File) (string, error) {
+	var n C.uint
+	err := ioctl(ptm.Fd(), syscall.TIOCGPTN, uintptr(unsafe.Pointer(&n)))
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("/dev/pts/%d", n), nil
+}
+
+func unlockpt(ptm *os.File) error {
+	var n C.uint
+	return ioctl(ptm.Fd(), syscall.TIOCSPTLCK, uintptr(unsafe.Pointer(&n)))
+}
+
+func ioctl(fd, request, argp uintptr) error {
+	_, _, err := syscall.Syscall(syscall.SYS_IOCTL, fd, request, argp)
+	if err != 0 {
+		return err
+	}
+	return nil
+}
