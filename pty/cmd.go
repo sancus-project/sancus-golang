0ccedb3a33d5c404b9d415f4eb0f6150710bd8b6
Alejandro Mery <amery@geeks.cl>
2015-05-17 00:30:30 +0100

    pty: StartCmd() and StartCommand()
    start a process on a pts and return the controlling pty
    
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

---
+++ pty/cmd.go
@@ -0,0 +1,41 @@
+package pty
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func StartCommand(name string, arg ...string) (*os.File, error) {
+	c := exec.Command(name, arg...)
+
+	return StartCmd(c)
+}
+
+func StartCmd(c *exec.Cmd) (*os.File, error) {
+	pty, pts, err := Open()
+	if err != nil {
+		return nil, err
+	}
+
+	// on success c will kept it held
+	defer pts.Close()
+
+	c.Stdin = pts
+	c.Stdout = pts
+	c.Stderr = pts
+
+	if c.SysProcAttr == nil {
+		c.SysProcAttr = &syscall.SysProcAttr{}
+	}
+	c.SysProcAttr.Setctty = true // pts is controlling terminal
+	c.SysProcAttr.Setsid = true  // new session
+
+	err = c.Start()
+	if err != nil {
+		pty.Close()
+		return nil, err
+	}
+
+	return pty, nil
+}
