ab32a92bb0717190fd908a4584b39f7261d64914
Alejandro Mery <amery@geeks.cl>
2015-06-07 23:17:30 +0200

    log: change NewError() to use errors.New()
    Signed-off-by: Alejandro Mery <amery@geeks.cl>

--- log/error.go
+++ log/error.go
@@ -1,34 +1,29 @@
 package log
 
 import (
+	"errors"
 	"fmt"
 )
-
-type ErrorString struct {
-	str string
-}
-
-func (e *ErrorString) Error() string {
-	return e.str
-}
 
 func NewError(str string, a ...interface{}) error {
 	if len(a) > 0 {
 		str = fmt.Sprintf(str, a...)
 	}
 
-	return &ErrorString{str}
+	return errors.New(str)
 }
 
 func (l *Logger) NewError(str string, a ...interface{}) error {
-	if l.tag != "" {
-		str = "%s: " + str
-		a = append([]interface{}{l.tag}, a...)
-	}
-
 	if len(a) > 0 {
+		if l.tag != "" {
+			str = "%s: " + str
+			a = append([]interface{}{l.tag}, a...)
+		}
+
 		str = fmt.Sprintf(str, a...)
+	} else if l.tag != "" {
+		str = l.tag + ": " + str
 	}
 
-	return &ErrorString{str}
+	return errors.New(str)
 }
